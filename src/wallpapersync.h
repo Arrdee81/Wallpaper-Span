7561087841b93a99a09a61d33c33998267505932
Arrdee <54414826+Arrdee81@users.noreply.github.com>
2026-05-07 08:01:43 -0400

    Refactor WallpaperSync class implementation
    Refactored WallpaperSync class methods and properties.

--- src/wallpapersync.h
+++ src/wallpapersync.h
@@ -2,35 +2,29 @@
 #define WALLPAPERSYNC_H
 
 #include <QObject>
-#include <QFileSystemWatcher>
 #include <QString>
 #include <qqmlregistration.h>
 
 class WallpaperSync : public QObject
 {
     Q_OBJECT
-    QML_ELEMENT
+    QML_NAMED_ELEMENT(WallpaperSync)
+    QML_SINGLETON
     Q_PROPERTY(QString currentImage READ currentImage WRITE setCurrentImage NOTIFY currentImageChanged)
-    
+
 public:
-    explicit WallpaperSync(QObject *parent = nullptr);
-    
-    QString currentImage() const;
-    void setCurrentImage(const QString &image);
-    
+    explicit WallpaperSync(QObject *parent = nullptr) : QObject(parent) {}
+    QString currentImage() const { return m_currentImage; }
+    void setCurrentImage(const QString &image) {
+        if (m_currentImage == image) return;
+        m_currentImage = image;
+        Q_EMIT currentImageChanged();
+    }
+
 Q_SIGNALS:
     void currentImageChanged();
-    
-private Q_SLOTS:
-    void onFileChanged(const QString &path);
-    
-private:
-    void readFromFile();
-    void writeToFile();
-    
-    QString m_currentImage;
-    QString m_syncFilePath;
-    QFileSystemWatcher *m_watcher;
-};
 
-#endif // WALLPAPERSYNC_H
+private:
+    QString m_currentImage;
+};
+#endif
